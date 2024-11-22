ad826992121eb515cfbfc83c773016b5c3c4c899
Pedro J. <pedrojluke87@gmail.com>
2024-11-22 15:51:37 +0100

    ADDING layers

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import Member from "./src/components/Member";
 
-export default function App() {
+const App = () => {
+  // State to manage tasks and user input
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+
+  // Function to add a new task
+  const addTask = () => {
+    if (task.trim() !== "") {
+      setTasks([...tasks, task]);
+      setTask(""); // Clear the input
+    }
+  };
+
+  // Function to delete a task by its index
+  const deleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>To-Do List</Text>
+      </View>
+
+      {/* Member Input */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a task..."
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <FontAwesome5 name="plus" size={24} color="green" onPress={addTask} />
+      </View>
+
+      {/* Member List */}
+      <ScrollView style={styles.tasks}>
+        {tasks.map((text, index) => (
+          <Member key={index} text={text} onDelete={() => deleteTask(index)} />
+        ))}
+      </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    padding: 20,
+    marginTop: 20,
+  },
+  header: {
+    marginTop: 20,
+    marginBottom: 20,
     alignItems: "center",
-    justifyContent: "center",
+  },
+  headerText: {
+    fontSize: 36,
+    fontWeight: "bold",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#777",
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+  },
+  tasks: {
+    marginTop: 30,
   },
 });
+
+export default App;
